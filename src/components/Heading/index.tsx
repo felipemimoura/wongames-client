d5414641d546714c1470864bcc34b330b0519e1c
Felipe Moura <felipemimoura@gmail.com>
2024-02-10 18:09:56 -0300

    feat: create component heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+export type HeadingProps = {
+  children: React.ReactNode
+  color?: 'white' | 'black'
+  leftLine?: boolean
+  lineBottom?: boolean
+}
+const Heading = ({ children, color, leftLine, lineBottom }: HeadingProps) => (
+  <S.Wrapper color={color} leftLine={leftLine} lineBottom={lineBottom}>
+    {children}
+  </S.Wrapper>
+)
+
+export default Heading
---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,40 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import { HeadingProps } from '.'
+import { DefaultTheme } from 'styled-components/dist/types'
+
+const wrapperModifier = {
+  leftLine: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      position: absolute;
+      left: 0;
+      bottom: -1rem;
+      content: '';
+      width: 5rem;
+      border: 0.4rem solid ${theme.colors.primary};
+    }
+  `
+}
+
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, color, leftLine, lineBottom }) => css`
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+
+    //Quando for maior do que 768px ele entra nesse media query
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+
+    ${leftLine && wrapperModifier.leftLine(theme)}
+    ${lineBottom && wrapperModifier.lineBottom(theme)}
+  `}
+`
