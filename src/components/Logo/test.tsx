6d3e8025e2ad609b1f52a9d0568c748bbbaf3e98
Felipe Moura <felipemimoura@gmail.com>
2024-02-10 08:17:38 -0300

    feat: finalizado componente logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,49 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import 'jest-styled-components'
 
 import Logo from '.'
+import { renderWithTheme } from '../../utils/tests/helper'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a black label', () => {
+    renderWithTheme(<Logo color="black" />)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+  it('should render a normal size by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem',
+      height: '3.3rem'
+    })
+  })
+  it('should render a large logo when is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem',
+      height: '5.9rem'
+    })
+  })
+  it('should render a big logo when in mobile', () => {
+    renderWithTheme(<Logo hideonmobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width:  768px)'
+      }
+    )
   })
 })
--- src/utils/tests/helper.tsx
+++ src/utils/tests/helper.tsx
@@ -1,12 +1,10 @@
-import { ThemeProvider } from 'styled-components'
 import { RenderResult, render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
 
-import theme from '@/styles/theme'
-import { PropsWithChildren } from 'react'
+import theme from '../../styles/theme'
+import React from 'react'
 
 //Componente para auxiliar nos teste.
 // Esse compnente renderiza o componente já passando as informações do tema
-export const renderWithTheme = ({
-  children
-}: PropsWithChildren): RenderResult =>
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
